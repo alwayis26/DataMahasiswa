ad6c2e1e4e801ad6038350c04a23abc4fa893e22
alwayiskarnis <133117494+wayis26@users.noreply.github.com>
2024-08-21 19:59:18 +0700

    fix-insertDataForm

--- create.php
+++ create.php
@@ -16,7 +16,9 @@
         padding: 0;
     }
 
-    tr, th, td{
+    tr,
+    th,
+    td {
         font-size: 12px;
     }
 
@@ -33,15 +35,12 @@
     }
 
     .la {
-        margin: 2rem 5rem;
+        margin: 1rem 5rem;
         display: flex;
         flex-direction: column;
         gap: 0.5rem;
         background-color: white;
         padding: 1rem;
-        -webkit-box-shadow: -1px 0px 80px -32px rgba(0, 0, 0, 0.75);
-        -moz-box-shadow: -1px 0px 80px -32px rgba(0, 0, 0, 0.75);
-        box-shadow: -1px 0px 80px -32px rgba(0, 0, 0, 0.75);
     }
 
     .dashboard {
@@ -74,95 +73,177 @@
         border: 1px solid #D4D4D4;
         padding: 3px 10px;
     }
-    input{
+
+    input {
         border: 1px solid #DBDBDB;
         border-radius: 5px;
         width: 100%;
         outline-color: #313EE9;
         padding: 5px;
     }
-    label{
+
+    label {
         color: #616161;
     }
-    .tab{
-        margin: 10px 0 0 0;
+
+    .tab {
+        margin: 5px 0 0 0;
         display: flex;
         flex-direction: row;
         width: 100%;
         gap: 1rem;
     }
-    .taba{
+
+    .taba {
         display: flex;
         flex-direction: column;
         gap: 0.5rem;
         width: 100%;
     }
-    .data{
+
+    .data {
         display: flex;
         flex-direction: column;
         gap: 0.5rem;
     }
-    button{
+
+    button {
         padding: 10px 0;
         border: none;
         background-color: #313EE9;
         color: white;
         margin: 10px 0 0 0;
     }
+
+    .su {
+        margin: 10px 0 0 0;
+    }
+
+    .alert {
+        margin: 20px 5rem;
+        background-color: #313EE9;
+        color: white;
+    }
     </style>
 
+    <!-- php -->
+    <?php
+        include 'loader/koneksi.php';
+
+        $sukses="";
+        $error="";
+        // INSERT DAN EDIT DATA
+        if (isset($_POST['submit'])) {
+            $nm = $_POST['nm'];
+            $tl = $_POST['tl'];
+            $as = $_POST['as'];
+            $ea = $_POST['ea'];
+            $fa = $_POST['fa'];
+            $pr = $_POST['pr'];
+            $nt = $_POST['nt'];
+            $ni = $_POST['ni'];
+
+            if ($nm && $tl && $as && $ea && $fa && $pr && $nt && $ni) {
+                $sql1 = "INSERT INTO mahasiswa
+                    (namaMahasiswa,tanggalLahir,asal,email,fakultas,prodi,nomerTelepon,nim)
+                    VALUES
+                    ('$nm','$tl','$as','$ea','$fa','$pr','$nt','$ni')";
+                $q1 = mysqli_query($koneksi, $sql1);
+                if ($q1) {
+                    echo '<div class="alert alert-success" role="alert" id="sukses">';
+                    echo 'Selamat, insert data berhasil';
+                    echo '</div>';
+                    echo '<script type="text/javascript">
+                        setTimeout(function(){
+                        window.location.href = "loader/loaderCD.php";
+                        }, 5000);
+                        </script>';
+                } else {
+                    $error = "gagal masuk data";
+                }
+            } else {
+                $error = "masukkan data dengan lengkap";
+            }
+        }
+        // INSERT DAN EDIT DATA
+    ?>
+
     <!-- dashboard -->
-    <div class="la">
-        <p class="dashboard">Insert Data Mahasiswa</p>
-        <div class="la-a">
-            <div class="la-aa">
-                <a href="loader/loaderCD.php">Dashboard</a>
-                <a href="loader/loaderDE.php">Edit And Delete Data</a>
+    <form action="" method="POST">
+        <div class="la">
+            <p class="dashboard">Insert Data Mahasiswa</p>
+            <div class="la-a">
+                <div class="la-aa">
+                    <a href="loader/loaderCD.php">Dashboard</a>
+                    <a href="loader/loaderDE.php">Edit And Delete Data</a>
+                </div>
+                <div class="la-aa">
+                    <a href="">@credit kami/</a>
+                </div>
             </div>
-            <div class="la-aa">
-                <a href="">@credit kami/</a>
+            <!-- PESAN ERROR -->
+            <script>
+            function hideDiv() {
+                var div = document.getElementById('error');
+                if (div) {
+                    div.style.display = 'none';
+                }
+            }
+
+            // Menghilangkan div setelah 5 detik (5000 milidetik)
+            setTimeout(hideDiv, 5000);
+            </script>
+            <?php
+                if ($error) {
+                ?>
+            <div class="alert alert-danger" role="alert" id="error">
+                <?php echo $error ?>
             </div>
+            <?php
+                }
+                ?>
+            <!-- PESAN ERROR -->
+            <div class="tab">
+                <div class="taba">
+                    <div class="data">
+                        <label for="">Nama Mahasiswa</label>
+                        <input type="text" name="nm" id="">
+                    </div>
+                    <div class="data">
+                        <label for="">Tanggal Lahir</label>
+                        <input type="date" name="tl" id="">
+                    </div>
+                    <div class="data">
+                        <label for="">Asal</label>
+                        <input type="text" name="as" id="">
+                    </div>
+                    <div class="data">
+                        <label for="">Email Aktif</label>
+                        <input type="text" name="ea" id="">
+                    </div>
+                </div>
+                <div class="taba">
+                    <div class="data">
+                        <label for="">Fakultas</label>
+                        <input type="text" name="fa" id="">
+                    </div>
+                    <div class="data">
+                        <label for="">Prodi</label>
+                        <input type="text" name="pr" id="">
+                    </div>
+                    <div class="data">
+                        <label for="">No Telp</label>
+                        <input type="text" name="nt" id="">
+                    </div>
+                    <div class="data">
+                        <label for="">NIM</label>
+                        <input type="text" name="ni" id="">
+                    </div>
+                </div>
+            </div>
+            <button name="submit">Insert Data</button>
         </div>
-        <div class="tab">
-            <div class="taba">
-                <div class="data">
-                    <label for="">Nama Mahasiswa</label>
-                    <input type="text" name="" id="">
-                </div>
-                <div class="data">
-                    <label for="">Tanggal Lahir</label>
-                    <input type="date" name="" id="">
-                </div>
-                <div class="data">
-                    <label for="">Asal</label>
-                    <input type="text" name="" id="">
-                </div>
-                <div class="data">
-                    <label for="">Email Aktif</label>
-                    <input type="text" name="" id="">
-                </div>
-            </div>
-            <div class="taba">
-                <div class="data">
-                    <label for="">Fakultas</label>
-                    <input type="text" name="" id="">
-                </div>
-                <div class="data">
-                    <label for="">Prodi</label>
-                    <input type="text" name="" id="">
-                </div>
-                <div class="data">
-                    <label for="">No Telp</label>
-                    <input type="text" name="" id="">
-                </div>
-                <div class="data">
-                    <label for="">NIM</label>
-                    <input type="text" name="" id="">
-                </div>
-            </div>
-        </div>
-        <button>Insert Data</button>
-    </div>
+    </form>
     <!-- dashboard -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
